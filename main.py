a945e476a3bb6b3b5e2e038072a19276640c0e6b
dakshQ2T <daksh.pote@quest2travel.com>
2026-05-15 16:35:55 +0530

    Refactor Flask app to support dynamic port

--- main.py
+++ main.py
@@ -176,5 +176,9 @@ def extract():
         return jsonify({"STATUS": f"❌ ERROR: {str(e)[:200]}", "File Name": file_name}), 500
 
 
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8000)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
